4013a26ef583e2d7244055fba2eaad666a4766cc
Andrej Kling <boxingprofi@web.de>
2021-01-15 18:13:21 +0100

    Initial Commit

---
+++ Folk/FolkApp.swift
@@ -0,0 +1,17 @@
+//
+//  FolkApp.swift
+//  Folk
+//
+//  Created by Andrej Kling on 15.01.21.
+//
+
+import SwiftUI
+
+@main
+struct FolkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
